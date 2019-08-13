a21ba8c2a52c36fcff831a5e1c42947f50495212
eric_jacques <eric_jacques@carrefourl.com>
2019-08-14 00:39:02 +0200

    seulement les post non supprimé

--- SQL/ExtractionDonnees.sql
+++ SQL/ExtractionDonnees.sql
@@ -8,11 +8,11 @@ SELECT p.body, p.title, p.tags, pty.Name
 FROM Posts as p
 inner join PostTypes as pty ON pty.Id = p.PostTypeId
 inner join (SELECT distinct PostId FROM PostTags) as pt on pt.PostId = p.Id
-/*inner join PostTags as pta on pta.PostId = p.id
-inner join Tags as t on t.id = pta.tagId*/
+left join (SELECT distinct PostId FROM PendingFlags WHERE FlagTypeId = 14) as pf on pf.PostId = p.Id
 WHERE pty.ID = 1
 AND p.AnswerCount > 0
 AND Nullif(p.AcceptedAnswerId,'') is null
 AND p.DeletionDate is null
 AND p.Tags is not null
+AND pf.PostId is null
 AND p.score > 0
