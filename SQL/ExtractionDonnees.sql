7ffa66cbfc3006218407553098d7a278d3dcca41
eric_jacques <eric_jacques@carrefourl.com>
2019-08-16 01:18:00 +0200

    Limitation des tags

--- SQL/ExtractionDonnees.sql
+++ SQL/ExtractionDonnees.sql
@@ -3,11 +3,30 @@
 -- Create date: 2019-08-10
 -- Description:	SQL request to execute at https://data.stackexchange.com/stackoverflow/query/ to generate data for the projetct
 -- ==========================================================================================
-
-SELECT p.body, p.title, p.tags, pty.Name 
+WITH Best_Tags AS (
+        SELECT Id, TagName, Count
+        FROM Tags
+        WHERE Count > 999
+        --ORDER BY Tags.Count DESC
+     )
+SELECT p.body, p.title
+		, p.tags
+		, (
+				SELECT TagName + ',' AS [text()]
+				FROM Best_Tags 
+				INNER JOIN PostTags ON Best_Tags.Id = PostTags.TagId
+				WHERE PostTags.PostId = p.Id
+                ORDER BY Best_Tags.Count DESC
+				FOR XML PATH('')
+		) AS SelectedTags
+		, pty.Name 
 FROM Posts as p
 inner join PostTypes as pty ON pty.Id = p.PostTypeId
-inner join (SELECT distinct PostId FROM PostTags) as pt on pt.PostId = p.Id
+inner join (
+	SELECT distinct PostId 
+	FROM PostTags
+    INNER JOIN Best_Tags ON Best_Tags.Id = PostTags.TagId
+) as pt on pt.PostId = p.Id
 left join (SELECT distinct PostId FROM PendingFlags WHERE FlagTypeId = 14) as pf on pf.PostId = p.Id
 WHERE pty.ID = 1
 AND p.AnswerCount > 0
@@ -16,3 +35,4 @@ AND p.DeletionDate is null
 AND p.Tags is not null
 AND pf.PostId is null
 AND p.score > 0
+ORDER BY p.score DESC
