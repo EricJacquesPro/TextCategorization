6398bf8630802d427dcb20a1586f0c4c679f9f87
eric_jacques <eric_jacques@carrefourl.com>
2019-08-21 00:14:52 +0200

    .

--- Python/TagText.py
+++ Python/TagText.py
@@ -7,12 +7,13 @@ class TagText:
     import sys
     
     from bs4 import BeautifulSoup
+    from collections import defaultdict
     from nltk import sent_tokenize, word_tokenize
-    from nltk.tokenize import word_tokenize
-    from nltk.corpus import stopwords
     from nltk.collocations import *
+    from nltk.corpus import stopwords
     from nltk.stem.snowball import SnowballStemmer
     from nltk.stem.wordnet import WordNetLemmatizer
+    from nltk.tokenize import word_tokenize
 
     try:
         stopwords = set(stopwords.words('english'))
@@ -49,7 +50,34 @@ class TagText:
         return df
     
     def preprocessing(self, text):
+        '''
+        Function to prepare data before 
+        '''
         clean_data = text
         clean_data = clean_data.lower()
         clean_data = self.BeautifulSoup(clean_data, 'html.parser').get_text()
+        #clean_data = self.re.sub(r'[^\w\s]', '', clean_data) # remove punctuation
         return clean_data
+
+    def manage_corpora(self, df_text):
+        corpora = self.defaultdict(list)
+        
+        tokenizer = self.nltk.RegexpTokenizer(r'\w+')
+
+        # create corpus
+        for id, row in df_text.head().iteritems():
+            corpora[id] += tokenizer.tokenize(row)
+        
+        stats, freq = dict(), dict()
+        print(corpora)
+        for k, v in corpora.iteritems():
+            freq[k] = fq = self.nltk.FreqDist(v)
+            stats[k] = {'total': len(v)} 
+        return (freq, stats, corpora)
+    
+    def freq_stats_corpora(self, data_question):
+
+        # count
+        freq, stats, corpora = self.manage_corpora(data_question)
+        df = self.pd.DataFrame.from_dict(stats, orient='index')
+        return df
